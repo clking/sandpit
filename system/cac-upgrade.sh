63ad25ba502b23322c552302c5b34128c3e996f5
clk <clk+git@gamesofa.com>
2017-03-31 14:01:44 +0800

    reordered screen windows

--- system/cac-upgrade.sh
+++ system/cac-upgrade.sh
@@ -109,8 +109,8 @@ echo generating a screen rc to open up and run
 sessionname upgrade
 
 screen top -c -d 1
-screen /tmp/upgrade.sh
 screen /bin/su clk -c /bin/sh -c 'cd /home/clk ; /bin/sh -c "\$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+screen /tmp/upgrade.sh
 HERE
 
 # use screen here instead of tmux because the upgrade breaks tmux from resuming
